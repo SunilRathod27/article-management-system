7037c079a5892b64a8cc4e8e586b7eaab97c8396
Sunil Rathod <shrathod515@gmail.com>
2026-07-02 01:10:19 +0530

    Wire up the root app component
    Adds NuxtRouteAnnouncer for accessible route change announcements and
    wraps every page in the error boundary.

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AppErrorBoundary from './components/common/AppErrorBoundary.vue'
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <AppErrorBoundary>
+      <NuxtPage />
+    </AppErrorBoundary>
+  </div>
+</template>
